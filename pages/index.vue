e033174cf22dbceefdee61fca2d8c2b81eec7f3a
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2022-07-15 22:21:11 -0500

    Refactored index with Sass 🎨️

--- pages/index.vue
+++ pages/index.vue
@@ -3,44 +3,51 @@
     <div class="containerOne">
       <h1>Salud mental para todos</h1>
     </div>
+    <div>
+      <div></div>
+      <div></div>
+    </div>
   </div>
 </template>
 
-<style>
+<style lang="scss">
+/* TODO: Create global header for all pages with Sass */
 .containerOne {
   display: flex;
   position: relative;
   height: 500px;
   width: 100%;
-  background: url("~/assets/img/index_img/main-background.jpg"),#99e9ff;;
+  background: url("~/assets/img/index_img/main-background.jpg"), #99e9ff;
   background-size: cover;
   background-position: center;
   align-items: center;
   justify-content: center;
+
+  h1 {
+    color: $clearColor;
+    text-align: center;
+    vertical-align: middle;
+    font-family: $globalFont;
+    font-size: 40px;
+    font-weight: bold;
+    z-index: 1;
+  }
 }
 
-.containerOne::before{
-  background-color: rgba(153, 191, 244, 0.595);
-  content: '';
+.containerOne::before {
+  background-color: rgba(153, 224, 244, 0.664);
+  content: "";
   display: block;
   height: 100%;
   position: absolute;
   width: 100%;
 }
-
-.containerOne h1 {
-  color: aliceblue;
-  text-align: center;
-  vertical-align: middle;
-  font-size: 50px;
-  z-index: 1;
-}
 </style>
 
-<script lang="ts">
-import Vue from 'vue'
+<script>
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'IndexPage',
-})
+  name: "IndexPage",
+});
 </script>
