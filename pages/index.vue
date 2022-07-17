da9ac17b62c1b45b6df06e0b69ae36959b2a51e7
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2022-07-16 21:46:23 -0500

    Implemented global Sass header component 🧑‍💻️

--- pages/index.vue
+++ pages/index.vue
@@ -13,34 +13,8 @@
 <style lang="scss">
 /* TODO: Create global header for all pages with Sass */
 .containerOne {
-  display: flex;
-  position: relative;
-  height: 500px;
-  width: 100%;
-  background: url("~/assets/img/index_img/main-background.jpg"), #99e9ff;
-  background-size: cover;
-  background-position: center;
-  align-items: center;
-  justify-content: center;
-
-  h1 {
-    color: $clearColor;
-    text-align: center;
-    vertical-align: middle;
-    font-family: $globalFont;
-    font-size: 40px;
-    font-weight: bold;
-    z-index: 1;
-  }
-}
-
-.containerOne::before {
-  background-color: rgba(153, 224, 244, 0.664);
-  content: "";
-  display: block;
-  height: 100%;
-  position: absolute;
-  width: 100%;
+@include globalHeader($customImage: url("~/assets/img/index_img/main-background.jpg"), 
+$backgroundColor: rgba(153, 224, 244, 0.664));
 }
 </style>
 
