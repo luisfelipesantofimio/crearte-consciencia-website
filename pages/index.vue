400e23fa2e44de364ad8bc1fb225a940670f95ea
Luis Felipe Santofimio <luisfelipesantofimio@gmail.com>
2022-07-19 22:04:30 -0500

    Implemented new grid layouts and advances on index

--- pages/index.vue
+++ pages/index.vue
@@ -5,37 +5,165 @@
     </div>
     <div class="containerTwo">
       <div>
-        <img src="~/assets/img/index_img/dark-logo.png" alt="Crearte Consciencia - logo oscuro">
+        <img
+          src="~/assets/img/index_img/dark-logo.png"
+          alt="Crearte Consciencia - logo oscuro"
+        />
       </div>
       <div class="textContainer">
         <h1>¿Qué es Crearte Consciencia?</h1>
-      <p>Somos un equipo de psicólogos dedicado a la prestación de servicios de salud mental para personas e instituciones. 
-        Trabajamos por el mejoramiento de la calidad de vida de las personas desde la psicología clínica, enfatizando nuestros 
-        esfuerzos en el aumento de la accesibilidad a salud mental para todos los colombianos. </p></div>
+        <p>
+          Somos un equipo de psicólogos dedicado a la prestación de servicios de
+          salud mental para personas e instituciones. Trabajamos por el
+          mejoramiento de la calidad de vida de las personas desde la psicología
+          clínica, enfatizando nuestros esfuerzos en el aumento de la
+          accesibilidad a salud mental para todos los colombianos.
+        </p>
+      </div>
     </div>
+    <div class="containerThree">
+      <div class="textContainer">
+        <h1>Integralidad</h1>
+        <p>
+          Somos un equipo de psicología integral. Esto significa que nuestra
+          perspectiva en torno al ser humano y su salud mental se nutre de
+          diferentes corrientes psicológicas, lo que nos permite dar una
+          atención especializada, acorde a las necesidades de cada consultante o
+          comunidad.
+        </p>
+      </div>
+      <div>
+        <img
+          src="~/assets/img/index_img/team.png"
+          alt="Grupo trabajando en equipo"
+        />
+      </div>
+    </div>
+    <div class="containerFour">
+      <div>
+        <img
+          src="~/assets/img/index_img/great.png"
+          alt="Dos personas chocando las manos"
+        />
+      </div>
+      <div>
+        <h1>Profesionales</h1>
+        <p>
+          Nuestro equipo de trabajo está conformado por psicólogos profesionales
+          de los más altos estándares éticos. En Crearte Consciencia siempre
+          estás en buenas manos.<br />
+
+          Conoce a nuestro equipo <nuxt-link to="/nosotros">aquí</nuxt-link>.
+        </p>
+      </div>
+    </div>
+    <div class="containerFive">
+      <div>
+        <h1>Salud mental para todos</h1>
+        <p>
+          Democratizamos el acceso a la salud mental. Por esta razón, evaluamos
+          si tus condiciones socioeconómicas son una dificultad para tu proceso,
+          ofreciendo descuentos personalizados si es necesario. También
+          implementamos proyectos sociales que mejoran la cobertura en salud
+          mental.<br />
+          ¡Contáctanos! Tu salud mental no debe ser un lujo.
+        </p>
+      </div>
+      <div><img src="~/assets/img/index_img/hug.png" alt="" /></div>
+    </div>
+    <section class="gridContainer">
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </section>
   </div>
 </template>
 
 <style lang="scss">
+.textContainer {
+  @include customText;
+}
 
-.container{
+.container {
   width: 100%;
   .containerOne {
-  @include globalHeader(
-    $customImage: url("~/assets/img/index_img/main-background.jpg"),
-    $backgroundColor: rgba(153, 224, 244, 0.664)
-  );
-}
+    @include globalHeader(
+      $customImage: url("~/assets/img/index_img/main-background.jpg"),
+      $backgroundColor: rgba(153, 224, 244, 0.664)
+    );
+  }
 
-.containerTwo {
-  @include bodyContentMain($clearText: true, $backgroundColor: white, $height: 400px, $imgHeight: 200);
+  .containerTwo {
+    @include bodyContentMain(
+      $textColor: $darkColor,
+      $backgroundColor: white,
+      $height: 400px,
+      $imgWidth: 200px
+    );
+  }
 
-  .textContainer{
-    @include customText;
+  .containerThree {
+    @include bodyContentMain(
+      $textColor: $clearColor,
+      $backgroundColor: $mainColor,
+      $height: 300,
+      $imgWidth: 700px
+    );
+  }
+
+  .containerFour {
+    @include bodyContentMain(
+      $textColor: $clearColor,
+      $backgroundColor: $secundaryColor,
+      $height: auto,
+      $imgWidth: 1000px
+    );
+    background: linear-gradient(
+      -177deg,
+      $mainColor 0% 20%,
+      $secundaryColor 20%
+    );
+    p {
+      padding-left: 100px;
+      padding-right: 100px;
+    }
+    a {
+      color: $darkColor;
+    }
+    a:hover {
+      color: rgb(86, 172, 243);
+    }
+  }
+
+  .containerFive {
+    @include bodyContentMain(
+      $textColor: $clearColor,
+      $backgroundColor: $secundaryColor,
+      $height: auto,
+      $imgWidth: 1200px
+    );
+    background: linear-gradient(
+      -177deg,
+      $secundaryColor 0% 20%,
+      $mainColor 20%
+    );
+    p {
+      padding-left: 100px;
+      padding-right: 100px;
+    }
   }
 }
-}
 
+.gridContainer {
+  @include featuresGrid();
+  background: white;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+}
 </style>
 
 <script>
